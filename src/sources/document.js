6004577b5ac46e3e2f4933fe5647cd47a10d5ca0
Riki Fridrich <riki@fczbkk.com>
2017-08-07 12:31:24 +0200

    feat: add sources for element content, value and checked state

--- src/sources/document.js
+++ src/sources/document.js
@@ -1,22 +1,93 @@
 import ElementCollector from 'element-collector';
 
 
-/**
- * Returns single matching element or `null` if no matching element is found.
- * @param {string} selector
- * @returns {Element|null}
- */
-export function element (selector) {
+function getElement(selector) {
   const collector = new ElementCollector(selector);
   return collector.get()[0] || null;
 }
 
-
-export function element_content (selector) {
-  // TODO
+function getElements(selector) {
+  const collector = new ElementCollector(selector);
+  return collector.get();
 }
 
 
+/**
+ * Returns single matching element or `null` if no matching element is found.
+ * @param {*} selector
+ * @returns {Element|null}
+ */
+export function element (selector) {
+  return getElement(selector);
+}
+
+/**
+ * Returns text content of an element and all its children. Returns `null` if no matching element is found.
+ * @param {*} selector
+ * @returns {string|null}
+ */
+export function element_content (selector) {
+  const found_element = getElement(selector);
+  if (found_element !== null) {
+    return found_element.textContent;
+  }
+  return null;
+}
+
+/**
+ * Returns value of an element (should be form field). Form fields that can hold multiple values (e.g. multi select, checkboxes) return array of values. Returns `null` if no matching element is found.
+ * @param {*} selector
+ * @returns {Array|string|null}
+ */
 export function element_value (selector) {
-  // TODO
+  const found_element = getElement(selector);
+  if (found_element) {
+
+    if (found_element.tagName === 'INPUT' && found_element.type === 'text') {
+      return found_element.value;
+    }
+
+    if (found_element.tagName === 'TEXTAREA') {
+      return found_element.value;
+    }
+
+    if (found_element.tagName === 'SELECT') {
+      const options_list = found_element.options;
+
+      if (found_element.multiple) {
+        return Object.keys(options_list)
+          .map(key => options_list[key])
+          .filter(item => item.selected)
+          .map(item => item.value);
+      } else {
+        const selected_index = found_element.selectedIndex;
+
+        if (options_list.length > 0 && selected_index > -1) {
+          return options_list[selected_index].value
+        }
+      }
+
+    }
+
+  }
+  return null;
+}
+
+
+/**
+ * Returns `true` if element is checked, otherwise `false`. Works only on CHECKBOX and RADIO inputs. All other matching elements will return `null`.
+ * @param {*} selector
+ * @returns {boolean|null}
+ */
+export function element_checked (selector) {
+  const found_element = getElement(selector);
+  if (
+    found_element
+    && found_element.tagName === 'INPUT'
+    && (found_element.type === 'checkbox' || found_element.type === 'radio')
+  ) {
+    return found_element.checked;
+  }
+
+  return null;
 }
--- test/source-document.spec.js
+++ test/source-document.spec.js
@@ -3,21 +3,35 @@ import * as x from './../src/sources/document.js';
 
 describe('document', function () {
 
-  describe('element', function () {
+  function createElement (parent = document.body, class_name = '') {
+    const elm = document.createElement('div');
+    parent.appendChild(elm);
+    elm.className = ['test', class_name].join(' ');
+    return elm;
+  }
 
-    function createElement (parent = document.body, class_name = '') {
-      const elm = document.createElement('div');
-      parent.appendChild(elm);
-      elm.className = ['test', class_name].join(' ');
-      return elm;
-    }
+  function createElementTag (tag = 'div', attributes = {}, parent = document.body) {
+    const elm = document.createElement(tag);
 
-    afterEach(function () {
-      const test_elements = document.querySelectorAll('.test');
-      Array.prototype.slice.apply(test_elements).forEach((elm) => {
-        elm.parentNode.removeChild(elm);
-      });
+    Object.keys(attributes).forEach(key => {
+      elm.setAttribute(key, attributes[key]);
     });
+
+    elm.classList.add('test');
+
+    parent.appendChild(elm);
+
+    return elm;
+  }
+
+  afterEach(function () {
+    const test_elements = document.querySelectorAll('.test');
+    Array.prototype.slice.apply(test_elements).forEach((elm) => {
+      elm.parentNode.removeChild(elm);
+    });
+  });
+
+  describe('element', function () {
 
     it('should return null if selector is missing', function () {
       expect(x.element()).toEqual(null);
@@ -37,6 +51,127 @@ describe('document', function () {
       const child_element = createElement(parent_element, 'child');
       expect(x.element('.child'))
         .toEqual(child_element);
+    });
+
+  });
+
+  describe('element content', function () {
+
+    it('should return `null` when no matching element is found', function () {
+      const result = x.element_content();
+      expect(result).toEqual(null);
+    });
+
+    it('should return empty string on empty element', function () {
+      const elm = createElement();
+      const result = x.element_content(elm);
+      expect(result).toEqual('');
+    });
+
+    it('should return element text content', function () {
+      const elm = createElement();
+      elm.innerHTML = 'aaa';
+      const result = x.element_content(elm);
+      expect(result).toEqual('aaa');
+    });
+
+    it('should return element text content including children', function () {
+      const elm = createElement();
+      elm.innerHTML = 'aaa <span>bbb</span>';
+      const result = x.element_content(elm);
+      expect(result).toEqual('aaa bbb');
+    });
+
+  });
+
+  describe('element value', function () {
+
+    it('should return `null` if no matching element is found', function () {
+      const result = x.element_value();
+      expect(result).toEqual(null);
+    });
+
+    it('should return `null` if element type is not recognized', function () {
+      const elm = createElement();
+      const result = x.element_value(elm);
+      expect(result).toEqual(null);
+    });
+
+    it('should return value of INPUT field', function () {
+      const elm = createElementTag('input', {value: 'aaa'});
+      const result = x.element_value(elm);
+      expect(result).toEqual('aaa');
+    });
+
+    it('should return value of TEXTAREA', function () {
+      const elm = createElementTag('textarea');
+      elm.innerHTML = 'aaa';
+      const result = x.element_value(elm);
+      expect(result).toEqual('aaa');
+    });
+
+    it('should return `null` if nothing is selected in SELECT', function () {
+      const elm = createElementTag('SELECT');
+      const result = x.element_value(elm);
+      expect(result).toEqual(null);
+    });
+
+    it('should return selected value of SELECT', function () {
+      const elm = createElementTag('SELECT');
+      elm.innerHTML = '' +
+        '<option value="aaa" selected></option>' +
+        '<option value="bbb"></option>';
+      const result = x.element_value(elm);
+      expect(result).toEqual('aaa');
+    });
+
+    it('should return array of selected values of multi SELECT', function () {
+      const elm = createElementTag('SELECT', {multiple: true});
+      elm.innerHTML = '' +
+        '<option value="aaa" selected></option>' +
+        '<option value="bbb"></option>' +
+        '<option value="ccc" selected></option>';
+      const result = x.element_value(elm);
+      expect(result).toEqual(['aaa', 'ccc']);
+    });
+
+  });
+
+  describe('element checked', function () {
+
+    it('should return `null` if no matching element is found', function () {
+      const result = x.element_checked();
+      expect(result).toEqual(null);
+    });
+
+    it('should return `null` if element type is not recognized', function () {
+      const elm = createElementTag('div');
+      const result = x.element_checked(elm);
+      expect(result).toEqual(null);
+    });
+
+    it('should return `true` if CHECKBOX is checked', function () {
+      const elm = createElementTag('input', {type: 'checkbox', checked: true});
+      const result = x.element_checked(elm);
+      expect(result).toEqual(true);
+    });
+
+    it('should return `false` if CHECKBOX is not checked', function () {
+      const elm = createElementTag('input', {type: 'checkbox'});
+      const result = x.element_checked(elm);
+      expect(result).toEqual(false);
+    });
+
+    it('should return `true` if RADIO is checked', function () {
+      const elm = createElementTag('input', {type: 'radio', checked: true});
+      const result = x.element_checked(elm);
+      expect(result).toEqual(true);
+    });
+
+    it('should return `false` if RADIO is not checked', function () {
+      const elm = createElementTag('input', {type: 'radio'});
+      const result = x.element_checked(elm);
+      expect(result).toEqual(false);
     });
 
   });
